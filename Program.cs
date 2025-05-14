e79db2f44f8ef1451d96a592b26363d6c17743c1
deathatred <maksym.linynskyi@gmail.com>
2025-05-14 20:37:43 +0300

    Battle logic update
    Battle logic almost ready

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Scissors_Rock_Paper
             int age = 0;
             string name = null;
 
-            
+
             Authorize(ref name, ref age);
             ShowPlayerStats(name, age, numbersOfRounds, wins, winrate);
             LaunchBattle(name);
@@ -68,26 +68,37 @@ namespace Scissors_Rock_Paper
             }
             else if (value == 1)
             {
-                Battle(name);      
+                Battle(name);
             }
 
         }
-        private static void Battle(string nickname)
+        private static void Battle(string nickname) //Add console write lines and win counter, battle logic is okay!
         {
-            Console.WriteLine("Now choose your weapon: 1.Scissors 2.Paper 3.Rock");
-            int value;
-            while (!int.TryParse(Console.ReadLine(), out value) || (value != 1 && value != 2 && value != 3))
-            {
-                Console.WriteLine("Invalid input. Please enter 1 or 2 or 3: ");
+            int roundCount = 0;
+            while (true)
+            {  
+                Console.Clear();
+                
+                Console.WriteLine($"ROUND {roundCount}\nNow choose your weapon: 1.Scissors 2.Paper 3.Rock");
+                int value;
+                while (!int.TryParse(Console.ReadLine(), out value) || (value != 1 && value != 2 && value != 3))
+                {
+                    Console.WriteLine("Invalid input. Please enter 1 or 2 or 3: ");
+                }
+                Random random = new Random();
+                int botChoise = random.Next(1, 4);
+                Console.WriteLine($"{value}, {botChoise}");
+                Console.WriteLine($"   {nickname}:           VS             BOT:");
+                Console.WriteLine(ShowBattleImage((value, botChoise),out bool win));
+                Console.ReadLine();
+                if (value != botChoise)
+                {
+                    roundCount++;
+                }
             }
-            Random random = new Random();
-            int botChoise = random.Next(1,4);
-            Console.WriteLine($"{value}, {botChoise}");
-            Console.WriteLine($"   {nickname}:           VS             BOT:");
-            Console.WriteLine(ShowBattleImage((value, botChoise)));
         }
 
-        private static string ShowBattleImage((int number, int secondNumber) input)
+        private static string ShowBattleImage((int number, int secondNumber) input, out bool win)
         {
             string scissorsVsScissors = @"
     _______                      _______
@@ -172,26 +183,35 @@ namespace Scissors_Rock_Paper
 
             switch (input)
             {
-                case (3,1):
+                case (3, 1):
+                    win = true;
                     return rockVsScissors;
-                case (3,2):
+                case (3, 2):
+                    win = false;
                     return rockVsPaper;
-                case (3,3):
+                case (3, 3):
+                    win = false;
                     return rockVsRock;
-                case (1,3):
+                case (1, 3):
+                    win = false;
                     return scissorsVsRock;
-                case (1,2):
+                case (1, 2):
+                    win = true;
                     return scissorsVsPaper;
-                case (1,1):
+                case (1, 1):
+                    win = false;
                     return scissorsVsScissors;
-                case (2,2):
+                case (2, 2):
+                    win = false;
                     return paperVsPaper;
-                case (2,3):
+                case (2, 3):
+                    win = true;
                     return paperVsRock;
-                case (2,1):
+                case (2, 1):
+                    win = false;
                     return paperVsScissors;
-                default: return rockVsScissors;
+                default:win = false; return rockVsScissors;
             }
         }
-        }
     }
+}
