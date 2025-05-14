17443f1ea9125868581929150e58137d9cc91a7d
deathatred <maksym.linynskyi@gmail.com>
2025-05-14 17:07:44 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Scissors_Rock_Paper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    _______\r\n---'   ____)____\r\n          ______)\r\n       __________)\r\n      (____)\r\n---.__(___)");
+        }
+    }
+}
